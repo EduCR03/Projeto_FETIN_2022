16686cc7e4d4c84f535fd5583c8208fdaf01cabd
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2022-05-18 22:26:48 -0300

    Player Movement
    Metendo a BRAZAAAA

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rigPlayer; // Referenciando o RigidDbody2D
-    private int health;
+    private Rigidbody2D rigPlayer; // Referenciando o RigidDbody2D
+    public int speed;
+    public int jumpForce;
     // Start is called before the first frame update
     void Start()
     {
-        //rigPlayer = GetComponent<Rigidbody2D>();
+        rigPlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Move();
+        Jump();
     }
 
     void Move()
     {
+        // retornando -1 esquerda, 1 direita
+        float movement = Input.GetAxis("Horizontal");
+        // Mudando a velocidade de acordo com o valor de movement 
+        rigPlayer.velocity = new Vector2(movement * speed, rigPlayer.velocity.y);
 
 
     }
 
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigPlayer.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 
 }
